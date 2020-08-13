d7d58428b99ea4934d1be41df2c1a0d721066840
wuzhanwei <zhanwei.wu@dbjtech.com>
2020-08-13 17:25:50 +0800

    feat: add leaf profile cmd

--- index.js
+++ index.js
@@ -11,5 +11,6 @@ program
 	.command('deploy [path]', 'Performs a deployment', { isDefault: true, executableFile: path.join(__dirname, 'deploy.js') })
 	.command('config', 'Config leaf', { executableFile: path.join(__dirname, 'config.js') })
 	.command('logs [name]', 'Get the serverless logs', { executableFile: path.join(__dirname, 'logs.js') })
+	.command('profile [name]', 'Set leaf config according to the profile', { executableFile: path.join(__dirname, 'profile.js') })
 
 program.parse(process.argv)
